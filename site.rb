f4309b0da95cce36485513b0df1a81e8b3466fb6
nishanthj14 <nishanthj14@gmail.com>
2016-05-12 16:32:04 -0600

    Update site.rb

--- site.rb
+++ site.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
- set :port, 4000
- set :bind, '0.0.0.0'
+ ENV['PORT'] ||= '4000'
+ set :port, ENV['PORT']
  get '/' do
    "I'm alive!"
  end
@@ -12,9 +12,6 @@ get '/italian' do
 get '/' do
    erb :index
  end
-
-ENV['PORT'] ||= '4000'
- set :port, ENV['PORT']
 
 get '/read_more' do
    erb :read_more
